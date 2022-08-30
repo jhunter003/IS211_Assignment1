6a0f2b19ff310338d02fd62ac7c25da9a29b17fe
jhunter003 <hunta4lif3@gmail.com>
2022-08-29 23:55:02 -0400

    added more data and modified for homework from is211example

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,21 +1,26 @@
-
 class Book:
-    """
-    Constructor of a Book's author class
-
-    :param author: The name of the author
-    :param title: the name of the book
-    """
     def __init__(self, author, title):
+        """
+        Constructor of an Author Class
+        :param author: The name of the author
+        :param title: The name of the book
+        """
         self.author = author
         self.title = title
         pass
-    
+
     def display(self):
-        """Display the book's author and title"""
-        print()
+        """Display the author's name and book title"""
+
+        print(f"I'm an author. My name is {self.author}.")
+        print(f"I wrote the book called {self.title}.")
         pass
 
 
 if __name__ == "__main__":
+    a1 = Book("first name1", "Book Title1")
+    a2 = Book("first name2", "Book Title2")
+
+    a1.display()
+    a2.display()
     pass
