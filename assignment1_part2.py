7fd33802075c7f13888e067ff82d8d435b69a277
jhunter003 <hunta4lif3@gmail.com>
2022-08-29 23:53:50 -0400

    add to git

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,9 +1,19 @@
 
 class Book:
+    """
+    Constructor of a Book's author class
+
+    :param author: The name of the author
+    :param title: the name of the book
+    """
     def __init__(self, author, title):
+        self.author = author
+        self.title = title
         pass
     
     def display(self):
+        """Display the book's author and title"""
+        print()
         pass
 
 
